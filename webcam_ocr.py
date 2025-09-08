fb098557ab0bc8949a54ebbc608293f7785235a6
N.A. <nuh.abdur.rahman.5@gmail.com>
2025-09-08 02:32:28 -0500

    got argostranslate working jp->en from webcam ocr text

--- webcam_ocr.py
+++ webcam_ocr.py
@@ -1,5 +1,7 @@
 import cv2
 from paddleocr import PaddleOCR
+import argostranslate.package
+import argostranslate.translate
 
 if __name__ == "__main__":
     vidcap = cv2.VideoCapture(0)
@@ -9,7 +11,7 @@ if __name__ == "__main__":
         print("webcam worked")
 
         #only english
-        ocr = PaddleOCR(lang='en')
+        ocr = PaddleOCR(lang='japan')
         result = ocr.predict(frame)
         recognized_texts = result[0]['rec_texts']
 
@@ -22,7 +24,27 @@ if __name__ == "__main__":
         print(final_result)
 
         cv2.imshow("frame", frame)
-        cv2.waitKey(0)
+        cv2.waitKey(3000)
+
+        #temp, will need to add something for choosing langauge later
+        from_code = "ja"
+        to_code = "en"
+
+        #argo installation
+        argostranslate.package.update_package_index()
+        available_packages = argostranslate.package.get_available_packages()
+        package_to_install = next(
+            filter(
+                lambda x: x.from_code == from_code and x.to_code == to_code, available_packages
+            )
+        )
+        argostranslate.package.install_from_path(package_to_install.download())
+
+        #translation
+        print("Translating")
+        translation = argostranslate.translate.translate(final_result, from_code, to_code)
+        print(translation)
+
     else:
         print("webcam failed.")
 
